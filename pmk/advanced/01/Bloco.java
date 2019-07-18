e1e83c6f3c7d63dd78a8fe4ad62f8c28775f84e0
Lucas Lopes Pereira <44032349+LucasPLopes@users.noreply.github.com>
2019-07-18 12:52:59 -0300

    Create Bloco.java

---
+++ pmk/advanced/01/Bloco.java
@@ -0,0 +1,19 @@
+public class Bloco{
+	
+	{
+		System.out.println("Dentro do bloco de inicialização");
+		
+	}
+	public Bloco(){
+		System.out.println("Dentro do construtor");
+	}
+}	
+
+/*
+	 new Bloco();
+	 
+	 output:
+		Dentro do bloco de inicialização
+		Dentro do construtor
+
+*/
