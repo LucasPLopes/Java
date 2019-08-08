1a2d4dcd420cf1f09b92e42d9374c5664e6685ab
Lucas Lopes <l.lopes.fga@gmail.com>
2019-08-08 14:12:52 -0300

    class Student2

---
+++ jsf-udemy/src/java/forms/Student2.java
@@ -0,0 +1,46 @@
+
+package forms;
+
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author lucas
+ */
+@ManagedBean
+public class Student2 {
+    
+    private String firstName;
+    private String lastName;
+    private String country;
+
+    public Student2() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    
+    
+    
+}
