f43b9405783a23488208d9deaa491f88fc935eac
Lucas Lopes <l.lopes.fga@gmail.com>
2019-08-13 23:59:24 -0300

    Main and UML

---
+++ Udemy/Java Design Patterns/Strategy/ex01/Main.java
@@ -0,0 +1,23 @@
+import controller.ScoreBoard;
+import model.Ballon;
+import model.Clown;
+import model.SquareBallon;
+
+public class Main{
+    public static void main(String[] args) {
+        ScoreBoard sb = new ScoreBoard();
+
+        System.out.println("Ballons taps score");
+        sb.algorithmBase = new Ballon();
+        sb.showScore(20, 2);
+
+        System.out.println("Clown taps score");
+        sb.algorithmBase = new Clown();
+        sb.showScore(20, 2);
+
+        System.out.println("SquareBallon taps score");
+        sb.algorithmBase = new SquareBallon();
+        sb.showScore(20, 2);
+
+    }
+}
