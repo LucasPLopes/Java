f720ad18eb83439a6b0bc38d0fbf01f1808595ed
Lucas Lopes <l.lopes.fga@gmail.com>
2019-08-19 21:03:35 -0300

    Use and explain the keyword volatile

---
+++ multithreading/lesson02/App.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Processor extends Thread {
+    //there is no way that variable running be ignored by thread
+    private volatile boolean running = true;
+    public void run(){
+        while (running) {
+            System.out.println("Hello");
+            try {
+                Thread.sleep(200);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void shutdown(){
+        running = false;
+    }
+}
+public class App{
+    public static void main(String[] args) {
+        //two threads are running, main and processor
+        Processor processor = new Processor();
+        processor.start();
+        System.out.println("Press return to stop ...");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        processor.shutdown();
+    }
+}
