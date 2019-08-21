8076990c5c456e64491037a3bf05e093719acb0c
Lucas Lopes <l.lopes.fga@gmail.com>
2019-08-21 12:09:43 -0300

    implementation and fixes

--- multithreading/lesson09/App.java
+++ multithreading/lesson09/App.java
@@ -1,5 +1,41 @@
 public class App{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        final Processor processor = new Processor();
+
+
+        Thread t1 = new Thread(new Runnable(){
         
+            @Override
+            public void run() {
+                try {
+                    processor.produce();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        
+        Thread t2 = new Thread(new Runnable(){
+        
+            @Override
+            public void run() {
+                try {
+                    processor.consume();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+        t1.start();
+        t2.start();
+        
+        try {
+            Thread.sleep(30000);
+        } catch (Exception e) {
+            
+        }
+        System.exit(0);
     }
 }
--- multithreading/lesson09/Processor.java
+++ multithreading/lesson09/Processor.java
@@ -1,27 +1,44 @@
 import java.util.LinkedList;
+import java.util.Random;
 
 public class Processor {
     private LinkedList<Integer> list = new LinkedList<Integer>();
-    private final Int LIMIT = 10;
+    private final int LIMIT = 10;
     private Object lock = new Object();
 
     public void produce() throws InterruptedException {
         int value = 0;
-        while (true) {
-            synchronized(lock){
-                list.add(value++);
+
+        while(true){
+
+        synchronized(lock){
+
+            while (list.size() == LIMIT) {    
+                lock.wait();
+            }
+        
+            list.add(value);
+            System.out.println("Producer added: " + value + " queue size is " + list.size());
+            value++;
+            lock.notify();
             }
         }
     }
 
     public void consume() throws InterruptedException {
-        while (true) {
+        Random random = new Random();
+        while (true) {  
 
             synchronized(lock){
+
+                while (list.size() == 0 ) {
+                    lock.wait();
+                }
                 System.out.print("List.size() = " + list.size());
-                int value = list.removeFirst();
-                System.out.println(" Value removed = " + value);
+                System.out.println(" Value removed = " + list.removeFirst());
+                lock.notify();
             }
+            Thread.sleep(random.nextInt(1000));
         }
     }
 }
