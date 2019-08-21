6d37c83ca72541ff743080d0a6123f7d5ba72a32
Lucas Lopes <l.lopes.fga@gmail.com>
2019-08-20 23:01:25 -0300

    Implementation of locks to block execute one thread instead two thread

--- multithreading/lesson04/Worker.java
+++ multithreading/lesson04/Worker.java
@@ -2,71 +2,80 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class Worker{
+public class Worker {
     private Random random = new Random();
+
+    private Object lock1 = new Object();
+    private Object lock2 = new Object();
     private List<Integer> list1 = new ArrayList<Integer>();
     private List<Integer> list2 = new ArrayList<Integer>();
-    
-    public synchronized void  stageOne(){
-        try {
-            Thread.sleep(1);
-        } catch (Exception e) {
-            e.printStackTrace();   
-        }
-        list1.add(random.nextInt(100));
-    }
-    public synchronized void stageTwo(){
-        try {
-            Thread.sleep(1);
-        } catch (Exception e) {
-            e.printStackTrace();   
-        }
-        list2.add(random.nextInt(100));
 
+    // public synchronized void stageOne(){
+    public void stageOne() {
+        synchronized (lock1) {
+            try {
+                Thread.sleep(1);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            list1.add(random.nextInt(100));
+        }
     }
-    public void process(){
+
+    public void stageTwo() {
+        synchronized (lock2) {
+            try {
+                Thread.sleep(1);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            list2.add(random.nextInt(100));
+        }
+    }
+
+    public void process() {
         int max = 1000;
         for (int i = 0; i < max; i++) {
             stageOne();
             stageTwo();
         }
     }
-    public void main(){
+
+    public void main() {
         System.out.println("Starting...");
-        
+
         long start = System.currentTimeMillis();
-        
-       Thread t1 =  new Thread( new Runnable(){
-        
+
+        Thread t1 = new Thread(new Runnable() {
+
             @Override
             public void run() {
-                    process();     
+                process();
             }
         });
-        Thread t2 =  new Thread( new Runnable(){
-        
+        Thread t2 = new Thread(new Runnable() {
+
             @Override
             public void run() {
-                    process();     
+                process();
             }
         });
 
         t1.start();
         t2.start();
 
-        try{
+        try {
             t1.join();
             t2.join();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
 
-        long end  = System.currentTimeMillis();
+        long end = System.currentTimeMillis();
 
-        System.out.println("Time take: "+(end - start));
-        System.out.println("List1.size(): "+ list1.size());
-        System.out.println("List2.size(): "+ list2.size());
+        System.out.println("Time take: " + (end - start));
+        System.out.println("List1.size(): " + list1.size());
+        System.out.println("List2.size(): " + list2.size());
 
     }
 }
