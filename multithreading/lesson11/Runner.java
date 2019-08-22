ddde18f1c8abc089e8f092826212f0e4b965005e
Lucas Lopes <l.lopes.fga@gmail.com>
2019-08-22 08:37:06 -0300

    Create method acquireLocks()

--- multithreading/lesson11/Runner.java
+++ multithreading/lesson11/Runner.java
@@ -8,13 +8,36 @@ public class Runner {
     private Account account2 = new Account();
     private Lock lock1 = new ReentrantLock();
     private Lock lock2 = new ReentrantLock();
+    
+    private void acquireLocks(Lock lock1,Lock lock2) throws InterruptedException{
+        while (true) {
+            //Acquire locks
+            boolean gotFirstLock = false;
+            boolean gotSecondLock = false;
+
+            try{    
+                gotFirstLock = lock1.tryLock();
+                gotSecondLock = lock2.tryLock();
+            }finally{
+                if(gotFirstLock && gotSecondLock)
+                    return;
+                
+                if(gotFirstLock)
+                    lock1.unlock();
+                
+                if(gotSecondLock)
+                    lock2.unlock();
+            }
+            Thread.sleep(1);
+        }
+    }
 
     public void firstThread() throws InterruptedException {
         Random rand = new Random();
 
         for (int i = 0; i < 10000; i++) {
-            lock1.lock();
-            lock2.lock();
+        
+            acquireLocks(lock1, lock2);
             try {
                 Account.transfer(account1, account2, rand.nextInt(100));
             } finally {
