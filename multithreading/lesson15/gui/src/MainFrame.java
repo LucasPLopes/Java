0cc07602ae5fbb7bf84b84f01c070b747cd0ebf0
Lucas Lopes <l.lopes.fga@gmail.com>
2019-08-28 12:06:23 -0300

    override process() method

--- multithreading/lesson15/gui/src/MainFrame.java
+++ multithreading/lesson15/gui/src/MainFrame.java
@@ -10,7 +10,7 @@ import javax.swing.SwingWorker;
 
 public class MainFrame extends javax.swing.JFrame {
 
-    private JLabel count1 = new JLabel("0");
+    private JLabel countLabel = new JLabel("0");
     private JLabel statusLabel = new JLabel("Task not completed");
     private JButton startBtn = new JButton("Start");
 
@@ -28,7 +28,7 @@ public class MainFrame extends javax.swing.JFrame {
         gc.gridy = 0;
         gc.weightx = 1;
         gc.weighty = 1;
-        add(count1, gc);
+        add(countLabel, gc);
         gc.gridx = 0;
         gc.gridy = 1;
         gc.weightx = 1;
@@ -54,15 +54,21 @@ public class MainFrame extends javax.swing.JFrame {
     }
 
     private void start() {
-        SwingWorker<Boolean, Void> worker = new SwingWorker<Boolean, Void>() {
+        SwingWorker<Boolean, Integer> worker = new SwingWorker<Boolean, Integer>() {
             @Override
             protected Boolean doInBackground() throws Exception {
                 for (int i = 0; i < 30; i++) {
                     Thread.sleep(100);
-                    count1.setText("Hello " + i);
+                    countLabel.setText("Hello " + i);
                     System.out.println("Hello " + i);
+                    publish(i);
                 }
                 return true;
+            }
+
+            @Override
+            protected void process(List<Integer> chunks) {
+                int value = chunks.get(chunks.size() -1);
             }
 
             @Override
@@ -74,12 +80,6 @@ public class MainFrame extends javax.swing.JFrame {
                     e.printStackTrace();
                 }
             }
-
-            @Override
-            protected void process(List<Void> chunks) {
-                super.process(chunks); //To change body of generated methods, choose Tools | Templates.
-            }
-
         };
         worker.execute();
     }
