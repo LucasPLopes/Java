494130baa76e67656ac8b79eb1b7fb355e8b7b2e
Lucas Lopes <l.lopes.fga@gmail.com>
2019-08-28 12:12:19 -0300

    updates

--- multithreading/lesson15/gui/src/MainFrame.java
+++ multithreading/lesson15/gui/src/MainFrame.java
@@ -9,21 +9,21 @@ import javax.swing.JLabel;
 import javax.swing.SwingWorker;
 
 public class MainFrame extends javax.swing.JFrame {
-
+    
     private JLabel countLabel = new JLabel("0");
     private JLabel statusLabel = new JLabel("Task not completed");
     private JButton startBtn = new JButton("Start");
-
+    
     public MainFrame() {
-
+        
     }
-
+    
     public MainFrame(String title) {
         super(title);
         setLayout(new GridBagLayout());
         GridBagConstraints gc = new GridBagConstraints();
         gc.fill = GridBagConstraints.NONE;
-
+        
         gc.gridx = 0;
         gc.gridy = 0;
         gc.weightx = 1;
@@ -39,38 +39,38 @@ public class MainFrame extends javax.swing.JFrame {
         gc.weightx = 1;
         gc.weighty = 1;
         add(startBtn, gc);
-
+        
         startBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent arg0) {
                 start();
             }
         });
-
-        setSize(500, 150);
+        
+        setSize(250, 400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
-
     }
-
+    
     private void start() {
         SwingWorker<Boolean, Integer> worker = new SwingWorker<Boolean, Integer>() {
             @Override
             protected Boolean doInBackground() throws Exception {
                 for (int i = 0; i < 30; i++) {
-                    Thread.sleep(100);
-                    countLabel.setText("Hello " + i);
+                    Thread.sleep(200);
                     System.out.println("Hello " + i);
                     publish(i);
                 }
                 return true;
             }
-
+            
             @Override
             protected void process(List<Integer> chunks) {
-                int value = chunks.get(chunks.size() -1);
+                Integer value = chunks.get(chunks.size() - 1);
+                countLabel.setText("Current value: " + value);
+                
             }
-
+            
             @Override
             protected void done() {
                 try {
