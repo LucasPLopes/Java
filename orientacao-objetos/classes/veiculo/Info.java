3e0df84ffbda1530ff50b4756e24cc3535487e1c
LucasPLopes <l.lopes.fga@gmail.com>
2019-12-13 13:10:56 -0200

    Classe Info

---
+++ orientacao-objetos/classes/veiculo/Info.java
@@ -0,0 +1,17 @@
+public class Info{
+    private String modelo;
+    private String fabricante;
+    private Integer ano;
+
+    public Info(String modelo, String fabricante, Integer ano) {
+        this.modelo = modelo;
+        this.fabricante = fabricante;
+        this.ano = ano;
+    }
+    @Override
+    public String toString() {
+        return "Info [ano=" + ano + ", fabricante=" + fabricante + ", modelo=" + modelo + "]";
+    }
+    
+    
+}
