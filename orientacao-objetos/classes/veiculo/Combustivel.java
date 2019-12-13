f59f3fd9cfa6c63757f37ae055e1326787668f0a
LucasPLopes <l.lopes.fga@gmail.com>
2019-12-13 13:11:38 -0200

    Enum Combustivel

---
+++ orientacao-objetos/classes/veiculo/Combustivel.java
@@ -0,0 +1,20 @@
+public enum Combustivel {
+    
+    ALCOOL(0),
+    GASOLINA(1),
+    FLEX(2), 
+    DIESEL(3),
+    ELETRICO(4),
+    HIBRIDO(5);
+
+    private Integer tipo;
+
+    private Combustivel(Integer tipo) {
+        this.tipo = tipo;
+    }
+
+    public Integer getTipo() {
+        return this.tipo;
+    }
+
+}
