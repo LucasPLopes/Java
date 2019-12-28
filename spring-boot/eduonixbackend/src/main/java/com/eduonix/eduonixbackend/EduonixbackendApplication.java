8ccf8e3c91d54c84103baade1f93818598b8e779
LucasPLopes <l.lopes.fga@gmail.com>
2019-12-28 15:56:31 -0200

    Eduonix start project: backend

---
+++ spring-boot/eduonixbackend/src/main/java/com/eduonix/eduonixbackend/EduonixbackendApplication.java
@@ -0,0 +1,13 @@
+package com.eduonix.eduonixbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduonixbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduonixbackendApplication.class, args);
+	}
+
+}
