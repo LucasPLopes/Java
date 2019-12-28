2a2136a0cdf57f72240e9719349ab0d2b8b69599
LucasPLopes <l.lopes.fga@gmail.com>
2019-12-28 16:39:23 -0200

    devdojo/essentials add class Student.

---
+++ spring-boot/essentials/src/main/java/br/com/devdojo/essentials/model/Student.java
@@ -0,0 +1,13 @@
+package br.com.devdojo.essentials.model;
+
+public class Student {
+    private String name;
+
+    public Student() {
+        name = "";
+    }
+
+    public Student(String name) {
+        this.name = name;
+    }
+}
