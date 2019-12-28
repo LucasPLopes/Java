15b6e4455eeb8384609a13ffdc0ead7ff60e3a4a
LucasPLopes <l.lopes.fga@gmail.com>
2019-12-28 16:13:55 -0200

    spring-boot/eduonix/ return modelin json format

--- spring-boot/eduonixbackend/src/main/java/com/eduonix/eduonixbackend/controller/ApiController.java
+++ spring-boot/eduonixbackend/src/main/java/com/eduonix/eduonixbackend/controller/ApiController.java
@@ -1,10 +1,14 @@
 package com.eduonix.eduonixbackend.controller;
 
 import java.util.Calendar;
+import com.eduonix.eduonixbackend.model.*;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class ApiController {
@@ -13,5 +17,9 @@ public class ApiController {
     public String getDate() {
         return Calendar.getInstance().getTime().toString();
     }
+    @RequestMapping(value="/api/getUser", produces = MediaType.APPLICATION_JSON_VALUE)
+    public User requestMethodName() {
+        return new User(7,"lucas");
+    }    
 
 }
