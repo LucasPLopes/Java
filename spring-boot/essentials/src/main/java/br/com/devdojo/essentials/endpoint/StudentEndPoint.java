c042ea0c7bc0a2d6331b9c32325f9c2f2a97abc7
LucasPLopes <l.lopes.fga@gmail.com>
2019-12-30 22:10:25 -0300

    devdojo/essentials refatora Annotations

--- spring-boot/essentials/src/main/java/br/com/devdojo/essentials/endpoint/StudentEndPoint.java
+++ spring-boot/essentials/src/main/java/br/com/devdojo/essentials/endpoint/StudentEndPoint.java
@@ -6,18 +6,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.devdojo.essentials.error.CustomTypeError;
 import br.com.devdojo.essentials.model.Student;
 import br.com.devdojo.essentials.utils.DateUtil;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
 
 @RestController
 @RequestMapping("student")
@@ -29,42 +29,41 @@ public class StudentEndPoint {
         this.dateUtil = dateUtil;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public ResponseEntity<List<Student>> listAll() {
         System.out.println("/student/list acesso: " + dateUtil.formatLocalDateToDatabaseStyle(LocalDateTime.now()));
         return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
     }
 
-    @RequestMapping(method=RequestMethod.GET, path= "/{id}")  
+    @GetMapping(path = "/{id}")
     public ResponseEntity<?> getStudentById(@PathVariable("id") final int id) {
         final Student student = new Student();
         student.setId(id);
-        final int index =  Student.studentList.indexOf(student);
-        if(index == -1)
-        {
+        final int index = Student.studentList.indexOf(student);
+        if (index == -1) {
             return new ResponseEntity<>(new CustomTypeError("Student not found"), HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(Student.studentList.get(index),HttpStatus.OK );    
+        return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
     }
-    @RequestMapping(method=RequestMethod.POST)
-        public ResponseEntity<?> save(@RequestBody Student student){
-            Student.studentList.add(student);
-            return new ResponseEntity<>(student,HttpStatus.OK);
-        }
 
-    @RequestMapping(method=RequestMethod.DELETE)
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody Student student) {
+        Student.studentList.add(student);
+        return new ResponseEntity<>(student, HttpStatus.OK);
+    }
+
+    @DeleteMapping
     public ResponseEntity<?> delete(@RequestBody Student student) {
         Student.studentList.remove(student);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @RequestMapping(method=RequestMethod.PUT)
-    public ResponseEntity<?> update(@RequestBody Student student){
+    @PutMapping
+    public ResponseEntity<?> update(@RequestBody Student student) {
         Student.studentList.remove(student);
         Student.studentList.add(student);
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
-    
-    
+
 }
