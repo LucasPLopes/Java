527447ca896946e8d39e62177193f942a6978b94
LucasPLopes <l.lopes.fga@gmail.com>
2020-01-04 12:31:02 -0300

    Spring Boot Essentials 12 - Tratamento de erros em REST pt 03 - Transações

--- spring-boot/essentials/src/main/java/br/com/devdojo/essentials/endpoint/StudentEndPoint.java
+++ spring-boot/essentials/src/main/java/br/com/devdojo/essentials/endpoint/StudentEndPoint.java
@@ -1,10 +1,10 @@
 package br.com.devdojo.essentials.endpoint;
 
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.com.devdojo.essentials.error.Mensagem;
 import br.com.devdojo.essentials.error.ResourceNotFoundException;
 import br.com.devdojo.essentials.model.Student;
@@ -48,6 +47,7 @@ public class StudentEndPoint {
     }
 
     @PostMapping
+    @Transactional(rollbackFor = Exception.class)
     public ResponseEntity<?> save(@RequestBody Student student) {
         return new ResponseEntity<>(studentDAO.save(student), HttpStatus.CREATED);
     }
