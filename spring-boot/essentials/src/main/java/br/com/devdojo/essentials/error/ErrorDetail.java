9fe52c2e2243a0b2e08e29464be3cb7912e83d6a
LucasPLopes <l.lopes.fga@gmail.com>
2020-01-04 23:52:13 -0300

    Spring Boot Essentials 14 - Tratamento de erros em REST pt 05 - Padronizando todos os erros Parte 1

--- spring-boot/essentials/src/main/java/br/com/devdojo/essentials/error/ErrorDetail.java
+++ spring-boot/essentials/src/main/java/br/com/devdojo/essentials/error/ErrorDetail.java
@@ -1,40 +1,102 @@
 package br.com.devdojo.essentials.error;
 
-public class ErrorDetail{
+public class ErrorDetail {
+  private String title;
+  private Integer status;
+  private String detail;
+  private Long timestamp;
+  private String developerMessage;
+
+
+  public static final class Builder {
     private String title;
     private Integer status;
     private String detail;
     private Long timestamp;
     private String developerMessage;
-    public String getTitle() {
-      return title;
+
+    private Builder() {
     }
-    public void setTitle(String title) {
+
+    public static Builder newBuilder() {
+      return new Builder();
+    }
+
+    public Builder title(String title) {
       this.title = title;
+      return this;
     }
-    public Integer getStatus() {
-      return status;
-    }
-    public void setStatus(Integer status) {
+
+    public Builder status(Integer status) {
       this.status = status;
+      return this;
     }
-    public String getDetail() {
-      return detail;
-    }
-    public void setDetail(String detail) {
+
+    public Builder detail(String detail) {
       this.detail = detail;
+      return this;
     }
-    public Long getTimestamp() {
-      return timestamp;
-    }
-    public void setTimestamp(Long timestamp) {
+
+    public Builder timestamp(Long timestamp) {
       this.timestamp = timestamp;
+      return this;
     }
-    public String getDeveloperMessage() {
-      return developerMessage;
+
+    public Builder developerMessage(String message) {
+      this.developerMessage = message;
+      return this;
     }
-    public void setDeveloperMessage(String developerMessage) {
-      this.developerMessage = developerMessage;
+
+    public ErrorDetail build() {
+      ErrorDetail errorDetail = new ErrorDetail();
+      errorDetail.setDeveloperMessage(developerMessage);
+      errorDetail.setTitle(title);
+      errorDetail.setDetail(detail);
+      errorDetail.setTimestamp(timestamp);
+      errorDetail.setStatus(status);
+      return errorDetail;
     }
+
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public Integer getStatus() {
+    return status;
+  }
+
+  public void setStatus(Integer status) {
+    this.status = status;
+  }
+
+  public String getDetail() {
+    return detail;
+  }
+
+  public void setDetail(String detail) {
+    this.detail = detail;
+  }
+
+  public Long getTimestamp() {
+    return timestamp;
+  }
+
+  public void setTimestamp(Long timestamp) {
+    this.timestamp = timestamp;
+  }
+
+  public String getDeveloperMessage() {
+    return developerMessage;
+  }
+
+  public void setDeveloperMessage(String developerMessage) {
+    this.developerMessage = developerMessage;
+  }
 
 }
--- spring-boot/essentials/src/main/java/br/com/devdojo/essentials/handler/RestExceptionHandler.java
+++ spring-boot/essentials/src/main/java/br/com/devdojo/essentials/handler/RestExceptionHandler.java
@@ -1,21 +1,24 @@
 package br.com.devdojo.essentials.handler;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import br.com.devdojo.essentials.error.ResourceNotFoundDetails;
-import br.com.devdojo.essentials.error.ResourceNotFoundException;
-import br.com.devdojo.essentials.error.ValidationErrorDetail;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
-
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import br.com.devdojo.essentials.error.ErrorDetail;
+import br.com.devdojo.essentials.error.ResourceNotFoundDetails;
+import br.com.devdojo.essentials.error.ResourceNotFoundException;
+import br.com.devdojo.essentials.error.ValidationErrorDetail;
 
 @ControllerAdvice
-public class RestExceptionHandler{
+public class RestExceptionHandler extends ResponseEntityExceptionHandler{
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException rnf){
         ResourceNotFoundDetails rnfDetails =  ResourceNotFoundDetails.Builder.newBuilder()
@@ -29,9 +32,8 @@ public class RestExceptionHandler{
             return new ResponseEntity<>(rnfDetails,HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> handleValidationErrorException(
-        MethodArgumentNotValidException manve    )
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(org.springframework.web.bind.MethodArgumentNotValidException manve, org.springframework.http.HttpHeaders headers, org.springframework.http.HttpStatus status, org.springframework.web.context.request.WebRequest request)
     {
 
         List<FieldError> fieldErrors =manve.getBindingResult().getFieldErrors();
@@ -50,5 +52,18 @@ public class RestExceptionHandler{
 
             return new ResponseEntity<>(rnfDetails,HttpStatus.BAD_REQUEST);
     }
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(
+			HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
 
+                ErrorDetail rnfDetails =  ErrorDetail.Builder.newBuilder()
+                .timestamp(new Date().getTime())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .title("Http Message error")
+                .detail("Http message not readable")
+                .developerMessage(ex.getClass().getName())
+                .build();
+    
+                return new ResponseEntity<>(rnfDetails,HttpStatus.BAD_REQUEST);
+	}
 }
