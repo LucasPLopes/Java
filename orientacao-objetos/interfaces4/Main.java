daf349b59724670858f60fb65151b874620f3a11
Lucas Lopes <l.lopes.fga@gmail.com>
2020-01-16 17:44:11 -0300

    Instanciaçao de interface

--- orientacao-objetos/interfaces4/Main.java
+++ orientacao-objetos/interfaces4/Main.java
@@ -3,8 +3,16 @@ public class Main{
 
         iMae imae = ()->System.out.println("Interface Mãe");
         iFilha ifilha = () ->System.out.println("Interface iFilha");
+        iFilha ifilha2 = new iFilha(){
+        
+            @Override
+            public void fun() {
+                System.out.println("Java 7 Interface iFilha");
+            }
+        };
         Util.funMae(imae);
         Util.funFilha(ifilha);
         Util. funMae(ifilha);
+        Util.funMae(ifilha2);
     }
 }
